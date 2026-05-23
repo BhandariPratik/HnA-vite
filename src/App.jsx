462cb2a01e8fb35e95695088c5375f61fdd44420
Pratik bhandari <pratikbhandari00722@gmail.com>
2026-05-23 18:35:28 +0530

    comment routes

--- src/App.jsx
+++ src/App.jsx
@@ -29,15 +29,16 @@ export default function App() {
       />
       <ScrollToTop />
       <Routes>
-        <Route path="*" element={<Home />} />
+
         <Route path="/" element={<Home />} />
-        <Route path="/practice" element={<Practice />} />
+        {/* <Route path="/practice" element={<Practice />} />
         <Route path="/process" element={<Process />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/projects/:category" element={<CategoryPage />} />
         <Route path="/project/:slug" element={<ProjectDetail />} />
         <Route path="/people" element={<People />} />
-        <Route path="/inquiry" element={<InquiryForm />} />
+        <Route path="/inquiry" element={<InquiryForm />} /> */}
+        <Route path="*" element={<Home />} />
       </Routes>
     </>
   );
